361264ac9e56fec01a56ab93d3d0224365a5bde0
Ved Bhonsle <98140094+Ved-Bhonsle@users.noreply.github.com>
2022-03-05 12:47:10 +0530

    Update piggy_bank.cpp

--- C++ Assignment/piggy_bank.cpp
+++ C++ Assignment/piggy_bank.cpp
@@ -1,10 +1,10 @@
 /*
 Suppose you have a Piggie Bank with an initial amount of Rs.50 and you have to add some
-more amount to it. Create a class &#39;AddAmount&#39; with a data member named &#39;amount&#39; with an
+more amount to it. Create a class "AddAmount" with a data member named 'amount' with an
 initial value of Rs. 50. Now make two constructors of this class as follows:
 1 - without any parameter - no amount will be added to the Piggie Bank
 2 - having a parameter which is the amount that will be added to the Piggie Bank
-Create an object of the &#39;AddAmount&#39; class and display the final amount in the Piggie Bank.
+Create an object of the 'AddAmount' class and display the final amount in the Piggie Bank.
 */
 
 #include<iostream>
